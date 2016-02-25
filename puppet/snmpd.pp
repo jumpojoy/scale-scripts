f6a84b61cdb4999d59b8e3bb11a63ef17012f0f3
Vasyl Saienko <vsaienko@mirantis.com>
2016-02-25 10:09:09 +0000

    Add firewall rule to allow snmp

--- puppet/snmpd.pp
+++ puppet/snmpd.pp
@@ -6,3 +6,10 @@ class { 'snmp':
   ro_community => 'public',
   ro_network   => ['172.18.170.44/32', '10.14.0.0/24']
 }
+
+firewall { '161 allow_snmp':
+    chain  => 'INPUT',
+    action => 'accept',
+    proto  => 'udp',
+    port   => '161',
+  }
