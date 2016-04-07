13342270e838bfbfcdcbd53c0aa6cf456f54c652
Vasyl Saienko <vsaienko@mirantis.com>
2016-04-07 07:22:50 +0000

    Some script improvements

--- scripts/create-vms.sh
+++ scripts/create-vms.sh
@@ -1,17 +1,74 @@
-function do_partitioning {
-  parted -s /dev/sda mkpart primary ext4 20940800s 146485247s
-  mkfs.ext4 /dev/sda4
-  mkdir /var/lib/libvirt/images2
-  mount /dev/sda4 /var/lib/libvirt/images2/
+function create_gp_vm {
+
+apt-get --yes --force-yes install virtinst
+qemu-img create -f qcow2 /var/lib/libvirt/images/gp.qcow2 11G
+cat << EOF > /etc/libvirt/qemu/gp.xml
+<domain type='kvm' id='6522'>
+  <name>gp</name>
+  <uuid>ccf363ff-a729-41f3-b9fa-b6d6acace318</uuid>
+  <memory unit='KiB'>1572864</memory>
+  <currentMemory unit='KiB'>1572864</currentMemory>
+  <vcpu placement='static'>2</vcpu>
+  <resource>
+    <partition>/machine</partition>
+  </resource>
+  <os>
+    <type>hvm</type>
+    <boot dev='network'/>
+    <boot dev='hd'/>
+    <bios rebootTimeout='5000'/>
+  </os>
+  <cpu mode='host-passthrough'>
+  </cpu>
+  <clock offset='utc'>
+    <timer name='rtc' tickpolicy='catchup' track='wall'>
+      <catchup threshold='123' slew='120' limit='10000'/>
+    </timer>
+    <timer name='pit' tickpolicy='delay'/>
+    <timer name='hpet' present='no'/>
+  </clock>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/bin/kvm-spice</emulator>
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2' cache='unsafe'/>
+      <source file='/var/lib/libvirt/images/gp.qcow2'/>
+      <target dev='vda' bus='virtio'/>
+      <serial>1627ff80b8144c21b077c47372130d7a</serial>
+      <alias name='virtio-disk0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0'/>
+    </disk>
+    <interface type='bridge'>
+      <source bridge='baremetal-br'/>
+      <mac address='00:16:3e:49:1d:11'/>
+    </interface>
+    <input type='mouse' bus='ps2'/>
+    <input type='keyboard' bus='ps2'/>
+    <graphics type='vnc' port='5943' autoport='yes' listen='0.0.0.0'>
+      <listen type='address' address='0.0.0.0'/>
+    </graphics>
+    <video>
+      <model type='vga' vram='9216' heads='1'/>
+      <alias name='video0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+    </video>
+    <memballoon model='virtio'>
+      <alias name='balloon0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x0'/>
+    </memballoon>
+  </devices>
+</domain>
+EOF
+
+virsh define /etc/libvirt/qemu/gp.xml
+
 }
 
 function clone_vms {
-  mv /var/lib/libvirt/images/gp1.img /var/lib/libvirt/images2/gp1.img
-  sed -i 's/images\//images2\//g' /etc/libvirt/qemu/gp1.xml
-  virsh define /etc/libvirt/qemu/gp1.xml
-
-  for i in {1..30}; do virt-clone -o gp1 -n gp1-clone-$i --auto-clone; done
+  for i in {1..120}; do virt-clone -o gp -n gp-clone-$i --auto-clone; done
 }
 
-do_partitioning
+create_gp_vm
 clone_vms
